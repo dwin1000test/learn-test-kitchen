0113844caf6edcbb181931469ee4e1b08cca8d41
Darren Win <dwin1000test@gmail.com>
2017-01-22 15:56:50 -0800

    added in CentOS ChefSpec test

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -6,15 +6,27 @@
 
 require 'spec_helper'
 
-describe 'learn_chef_apache2::default' do
-  context 'When all attributes are default, on an unspecified platform' do
+describe 'learn-test-kitchen::default' do
+  context 'When attributes are on CentOs 7' do
     let(:chef_run) do
-      runner = ChefSpec::ServerRunner.new
+      runner = ChefSpec::ServerRunner.new(platform: 'centos', version: '7.2.1511')
       runner.converge(described_recipe)
     end
 
     it 'converges successfully' do
       expect { chef_run }.to_not raise_error
+    end
+
+    it 'installs httpd' do
+      expect(chef_run).to install_package 'httpd'
+    end
+ 
+    it 'enables the httpd service' do
+      expect(chef_run).to enable_service 'httpd'
+    end
+
+    it 'starts the httpd service' do
+      expect(chef_run).to start_service 'httpd'
     end
   end
 end
