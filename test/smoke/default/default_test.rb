b96b142e79308a5b2cad830d98acc65998581fe1
Darren Win <dwin1000test@gmail.com>
2017-01-22 14:01:27 -0800

    added in Inspec tests

--- test/smoke/default/default_test.rb
+++ test/smoke/default/default_test.rb
@@ -12,7 +12,22 @@ unless os.windows?
   end
 end
 
+describe package 'apache2' do
+    it { should be_installed }
+end
+
+describe service 'apache2' do
+    it { should be_enabled }
+    it { should be_running }
+end
+
 describe port(80) do
   it { should be_listening }
-  skip 'This is an example test, replace with your own test.'
+  #skip 'This is an example test, replace with your own test.'
 end
+
+describe command 'curl localhost' do
+    its('stdout') { should match /Hello World/ }
+end
+
+
