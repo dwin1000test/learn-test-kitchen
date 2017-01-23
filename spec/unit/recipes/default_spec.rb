d7ecd309ca7e04d39a13142954f773c1fc637056
Darren Win <dwin1000test@gmail.com>
2017-01-22 21:39:28 -0800

    refactore Chefspec code to reduce redundancy

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -6,51 +6,34 @@
 
 require 'spec_helper'
 
+shared_examples 'learn-test-kitchen' do |platform, version, package, service|
+    context "when run on #{platform} #{version}" do
+        let(:chef_run) do
+            runner = ChefSpec::ServerRunner.new(platform: platform, version: version)
+            runner.converge(described_recipe)
+        end
+
+        it 'converges successfully' do
+            expect { chef_run }.to_not raise_error
+        end
+
+        it "installs #{package}" do
+            expect(chef_run).to install_package package
+        end
+
+        it "enables the #{service} service" do
+            expect(chef_run).to start_service service
+        end
+    end
+end
+
 describe 'learn-test-kitchen::default' do
-  context 'When attributes are on CentOs 7' do
-    let(:chef_run) do
-      runner = ChefSpec::ServerRunner.new(platform: 'centos', version: '7.2.1511')
-      runner.converge(described_recipe)
-    end
+    platforms = {
+        'centos' => ['7.2.1511', 'httpd', 'httpd'],
+        'ubuntu' => ['14.04', 'apache2', 'apache2']
+    }
 
-    it 'converges successfully' do
-      expect { chef_run }.to_not raise_error
+    platforms.each do |platform, platform_data|
+        include_examples 'learn-test-kitchen', platform, *platform_data
     end
-
-    it 'installs httpd' do
-      expect(chef_run).to install_package 'httpd'
-    end
- 
-    it 'enables the httpd service' do
-      expect(chef_run).to enable_service 'httpd'
-    end
-
-    it 'starts the httpd service' do
-      expect(chef_run).to start_service 'httpd'
-    end
-  end
-
-
-  context 'When attributes are on Ubuntu 14.04' do
-    let(:chef_run) do
-      runner = ChefSpec::ServerRunner.new(platform: 'ubuntu', version: '14.04')
-      runner.converge(described_recipe)
-    end
-
-    it 'converges successfully' do
-      expect { chef_run }.to_not raise_error
-    end
-
-    it 'installs apache2' do
-      expect(chef_run).to install_package 'apache2'
-    end
-
-    it 'enables the apache2 service' do
-      expect(chef_run).to enable_service 'apache2'
-    end
-
-    it 'starts the apache2 service' do
-      expect(chef_run).to start_service 'apache2'
-    end
-  end
 end
